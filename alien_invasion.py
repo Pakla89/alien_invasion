727e3177af5320d5036011b91b7d0677ce8e6878
Pakla89 <pakla21@gmail.com>
2024-12-14 22:21:40 +0100

    Adding spaceship

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,8 +1,12 @@
-import sys
+import sys, os
 
 import pygame
 
 from settings import Settings
+from ship import Ship
+
+os.chdir(os.path.dirname(__file__))
+print("Current working directory:", os.getcwd())
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior."""
@@ -16,6 +20,8 @@ class AlienInvasion:
             (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
+        self.ship = Ship(self)
+
         # Set the background color.
         self.bg_color = (230, 230, 230)
 
@@ -28,6 +34,7 @@ class AlienInvasion:
                     sys.exit()
             # Redraw the screen during each pass through the loop.
             self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
 
             # Make the most recently drawn screen visible.
             pygame.display.flip()

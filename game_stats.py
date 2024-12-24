38d08fe516e01ecea0aad814bd286a85d19964d2
Pakla89 <pakla21@gmail.com>
2024-12-24 13:59:41 +0100

    Load high score from high_score.json file

--- game_stats.py
+++ game_stats.py
@@ -1,3 +1,7 @@
+import os,json
+
+os.chdir(os.path.dirname(__file__))
+
 class GameStats:
     """Track statistics for Alien Invasion."""
 
@@ -6,10 +10,12 @@ class GameStats:
         self.settings = ai_game.settings
         self.reset_stats()
         # High score should never be reset.
-        self.high_score = 0
+        self.high_score = json.load(high_score_read)
 
     def reset_stats(self):
         """Initialize statistics that can change during the game."""
         self.ships_left = self.settings.ship_limit
         self.score = 0
         self.level = 1
+
+high_score_read = open('high_score.json')
